1b2c41131baeb3a63706d9bc14bdd307fb6c7091
kryozdemir <korayozdemir3455@gmail.com>
2024-03-24 02:45:05 +0300

    update3

--- src/conf.js
+++ src/conf.js
@@ -8,7 +8,7 @@ mkdirp.sync(hotelDir)
 // Defaults
 const defaults = {
   port: 2010,
-  host: '127.0.0.2',
+  host: '127.0.0.4',
   timeout: 5000,
   tld: 'localhost',
   // Replace with your network proxy IP (1.2.3.4:5000) if any
